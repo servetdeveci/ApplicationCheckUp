77073fb2f38e0fee1770d8f11baa601ad9f9f87c
Servet DEVECİ <servetdeveci@outlook.com>
2022-01-10 23:14:20 +0300

    Identity kaldırıldı

--- ApplicationHealth/MvcWebUI/AccountModel/UsersModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ApplicationHealth.MvcWebUI.AccountModel
-{
-    public class UsersModel
-   {
-      public bool _ajax { get; set; }
-      public int FirmId { get; set; }
-   }
-}
--- ApplicationHealth/MvcWebUI/ManageModel/GenerateRecoveryCodesModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ApplicationHealth.MvcWebUI.AccountModel.ManageModel
-{
-    public class GenerateRecoveryCodesModel
-   {
-      public string[] RecoveryCodes { get; set; }
-      public string StatusMessage { get; set; }
-   }
-}
